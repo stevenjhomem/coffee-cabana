09ed314b0e614ba22c8839b5df2cc2fb048e86b7
Steven J. Homem <sjhomem@outlook.com>
2025-07-02 10:11:37 -0500

    brighter

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -84,8 +84,6 @@ export default function HeroSection({ locale = 'pt' }: HeroSectionProps) {
             backgroundImage: `url('/images/coffee/Banana_EcoCamp-02.jpg')`
           }}
         />
-        {/* Dark overlay for better text readability */}
-        <div className="absolute inset-0 bg-black/30"></div>
       </div>
 
       {/* Hero Content */}
